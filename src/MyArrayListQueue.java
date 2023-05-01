e58a80c40077c6a3e94c02e5a09381e268ea15d1
Asqar <ahri@nine-tails.ru>
2023-05-01 23:35:18 +0600

    creating MyArrayListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,3 +1,18 @@
-public class MyArrayListQueue
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+/**
+ * A queue implementation based on ArrayList.
+ */
+public class MyArrayListQueue<T extends Comparable>
 {
+	private MyArrayList<T> list;
+	
+	/**
+	 * Constructs an empty queue.
+	 */
+	public MyArrayListQueue(int size)
+	{
+		list = new MyArrayList<T>(size);
+	}
 }
