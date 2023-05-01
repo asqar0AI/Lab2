dcb49158d0a8eb1eecd0eb578fa974dfd8ab8b26
Asqar <ahri@nine-tails.ru>
2023-05-01 22:39:07 +0600

    started implementing MyLinkedListQueue, done enqueue method

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,4 +1,14 @@
-public class MyLinkedListQueue
+public class MyLinkedListQueue<T extends Comparable>
 {
-
+	private MyLinkedList<T> list;
+	
+	public MyLinkedListQueue()
+	{
+		list = new MyLinkedList<>();
+	}
+	
+	public void enqueue(T item)
+	{
+		list.add(item);
+	}
 }
