c0100628523dc67e672071e0eda06fa8c2710f02
Asqar <ahri@nine-tails.ru>
2023-05-01 21:26:36 +0600

    implemented push method of custom LinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,3 +1,15 @@
-public class MyLinkedListStack
+public class MyLinkedListStack<T extends Comparable>
 {
+	private MyLinkedList<T> list;
+	public MyLinkedListStack()
+	{
+		list = new MyLinkedList<>();
+	}
+	
+	public void push(T item)
+	{
+		// adds element to the top
+		list.add(item, 0);
+	}
+	
 }
