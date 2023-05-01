880e490ebb05eff1d216d6529f9375820c7fc46f
Asqar <ahri@nine-tails.ru>
2023-05-01 23:11:10 +0600

    implemented push() method

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,3 +1,18 @@
-public class MyArrayListStack
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+public class MyArrayListStack<T extends Comparable>
 {
+	private MyArrayList<T> list;
+	
+	/**
+	 * Constructs an empty stack.
+	 */
+	public MyArrayListStack(int size)
+	{
+		list = new MyArrayList<T>(size);
+	}
+	public void push(T item)
+	{
+		list.add(item);
+	}
 }
