72bacfb0a185dabe7f21977de67e12be58b3f256
Asqar <ahri@nine-tails.ru>
2023-05-17 04:35:20 +0600

    implementing blank methods

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,48 @@
-public class MyHashTable
+import Interfaces.MyHashTableInterface;
+
+public class MyHashTable<V, K extends Comparable> implements MyHashTableInterface
 {
+	private class HashNode<K, V>
+	{
+	
+	}
+	
+	private HashNode<K, V>[] chainArray;
+	private int              M = 11;
+	private int              size;
+	@Override
+	public int hash(Object key)
+	{
+		return 0;
+	}
+	
+	@Override
+	public void put(Object key, Object value)
+	{
+	
+	}
+	
+	@Override
+	public Object get(Object key)
+	{
+		return null;
+	}
+	
+	@Override
+	public Object remove(Object key)
+	{
+		return null;
+	}
+	
+	@Override
+	public boolean constraints(Object value)
+	{
+		return false;
+	}
+	
+	@Override
+	public Object getKey(Object value)
+	{
+		return null;
+	}
 }
