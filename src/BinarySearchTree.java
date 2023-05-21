2d3d36811d4b0714053cb86d35e8f4e0ceddd9c3
Asqar <ahri@nine-tails.ru>
2023-05-22 04:43:40 +0600

    implementing nodes

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,4 +1,15 @@
 public class BinarySearchTree<K extends Comparable<K>, V>{
+    private Node root;
 
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
 
 }
