43b24a792aefc3cb2e96a9d1172057c289e22470
Asqar <ahri@nine-tails.ru>
2023-05-22 06:14:05 +0600

    in-code documentation written

--- src/MyBinarySearchTree.java
+++ src/MyBinarySearchTree.java
@@ -4,20 +4,34 @@ import java.util.List;
 public class MyBinarySearchTree<K extends Comparable<K>, V>{
     private Node root;
 
+    /**
+     * Represents a node in the binary search tree.
+     */
     private class Node {
         private K key;
         private V val;
         private Node left, right;
 
+        /**
+         * Creates a new instance of the Node class.
+         */
         public Node(K key, V val) {
             this.key = key;
             this.val = val;
         }
     }
+
+    /**
+     * Inserts a key-value pair into the binary search tree.
+     */
     public void put(K key, V val)
     {
         root = put(root, key, val);
     }
+
+    /**
+     * Private helper method to recursively insert a key-value pair into the binary search tree.
+     */
     private Node put(Node node, K key, V val)
     {
         if(node == null){
@@ -34,11 +48,17 @@ public class MyBinarySearchTree<K extends Comparable<K>, V>{
         return node;
     }
 
+    /**
+     * Retrieves the value associated with the specified key.
+     */
     public V get(K key){
         Node node = get(root, key);
         return node != null ? node.val : null;
     }
 
+    /**
+     * Private helper method to recursively search for a node with the specified key.
+     */
     private Node get(Node node, K key) {
         if (node == null){
             return null;
@@ -53,6 +73,9 @@ public class MyBinarySearchTree<K extends Comparable<K>, V>{
         return node;
     }
 
+    /**
+     * Finds the minimum key in the binary search tree.
+     */
     public Node findMin(Node node) {
         while (node.left != null) {
             node = node.left;
@@ -60,10 +83,16 @@ public class MyBinarySearchTree<K extends Comparable<K>, V>{
         return node;
     }
 
+    /**
+     * Deletes the node with the specified key from the binary search tree.
+     */
     public void delete(K key) {
         root = delete(root, key);
     }
 
+    /**
+     * Private helper method to recursively delete the node with the specified key from the binary search tree.
+     */
     private Node delete(Node node, K key) {
         if (node == null) {
             return null;
@@ -92,12 +121,18 @@ public class MyBinarySearchTree<K extends Comparable<K>, V>{
         return node;
     }
 
-    public Iterable<K> iterator() {
+    /**
+     * Returns an iterator that iterates over the keys in ascending order.
+     */    public Iterable<K> iterator() {
         List<K> keys = new ArrayList<>();
         inorderTraversal(root, keys);
         return keys;
     }
 
+    /**
+     * Private helper method to perform inorder traversal of the binary search tree.
+     * Collects the keys in ascending order into the specified list.
+     */
     private void inorderTraversal(Node node, List<K> keys) {
         if (node == null)
             return;
@@ -106,10 +141,16 @@ public class MyBinarySearchTree<K extends Comparable<K>, V>{
         inorderTraversal(node.right, keys);
     }
 
+    /**
+     * Returns the number of nodes in the binary search tree.
+     */
     public int size() {
         return size(root);
     }
 
+    /**
+     * Private helper method to recursively calculate the number of nodes in the binary search tree.
+     */
     private int size(Node node) {
         if (node == null) {
             return 0;
