dae25c5d145ff88c0d5cccf44bc340b48b036b86
Asqar <ahri@nine-tails.ru>
2023-05-31 21:10:02 +0600

    created constructor for Vertex

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class MyGraph {
-    private class Vertex {
+    private class Vertex<V> {
+        private V data;
+        private Map<Vertex<V>, Double> adjacentVertices;
 
+        public Vertex(V data) {
+            this.data = data;
+            adjacentVertices = new HashMap<>();
+        }
     }
 
 }
